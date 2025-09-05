c30ded0b785c08eaf47d1e7201705657ea73c098
Fred Tombs <fred@zeroasic.com>
2025-09-05 09:49:08 -0400

    next remove D

--- ARCHITECTURE/Z1010/DSP/zeroasic_dsp_map.v
+++ ARCHITECTURE/Z1010/DSP/zeroasic_dsp_map.v
@@ -48,54 +48,13 @@ module \$__MUL18X18 (input [17:0] A, input [17:0] B, output [39:0] Y);
 		// resetn ()
 
 		// delete
-		// .DOTP(1'b0), 
-		// .SIMD(1'b0), 
-		// .OVFL_CARRYOUT_SEL(1'b0), 
-		// .AL_N(1'b1),
-		// .A_EN(1'b1),
-		// .B_EN(1'b1),
 
 		.D(18'b0),
 		.D_BYPASS(1'b1),
 		.D_ARST_N(1'b1),
 		.D_SRST_N(1'b1),
 		.D_EN(1'b1),
-		
-		// .CARRYIN(1'b0),
 
-		// .C_EN(1'b1),
-
-		// .P_BYPASS(1'b1),
-		// .P_SRST_N(1'b1),
-		// .P_EN(1'b1),
-
-		// .PASUB(1'b0),
-		// .PASUB_BYPASS(1'b1),
-		// .PASUB_AD_N(1'b0),
-		// .PASUB_SL_N(1'b1),
-		// .PASUB_SD_N(1'b0),
-		// .PASUB_EN(1'b1),
-
-		// .CDIN_FDBK_SEL(2'b00),
-		// .CDIN_FDBK_SEL_BYPASS(1'b1),
-		// .CDIN_FDBK_SEL_AD_N(2'b00),
-		// .CDIN_FDBK_SEL_SL_N(1'b1),
-		// .CDIN_FDBK_SEL_SD_N(2'b00),
-		// .CDIN_FDBK_SEL_EN(1'b1),
-
-		// .ARSHFT17(1'b0),
-		// .ARSHFT17_BYPASS(1'b1),
-		// .ARSHFT17_AD_N(1'b0),
-		// .ARSHFT17_SL_N(1'b1),
-		// .ARSHFT17_SD_N(1'b0),
-		// .ARSHFT17_EN(1'b1),
-
-		.SUB(1'b0),
-		.SUB_BYPASS(1'b1),
-		.SUB_AD_N(1'b0),
-		.SUB_SL_N(1'b1),
-		.SUB_SD_N(1'b0),
-		.SUB_EN(1'b1)
 
 	);
 	assign Y = P_48;
