c0568f55279d996ca43ae75b073cf695c035f840
Fred Tombs <fred@zeroasic.com>
2025-09-09 09:36:42 -0400

    pm, map_mode.v cleanup

--- ARCHITECTURE/Z1010/DSP/zeroasic_dsp_map_mode.v
+++ ARCHITECTURE/Z1010/DSP/zeroasic_dsp_map_mode.v
@@ -11,22 +11,22 @@ module MAE #(
 (
   input [17:0] A,
   input A_BYPASS,
-  input A_EN, // do not remove
+  input A_EN,
   input A_SRST_N,
   input [17:0] B,
   input B_BYPASS,
-  input CDIN_FDBK_SEL, // REMOVE if you want i guess
-  input B_EN,  // do not remove
+  input CDIN_FDBK_SEL,
+  input B_EN,
   input B_SRST_N,
   input [39:0] C,
   input CLK,
   input C_ARST_N,
-  input C_BYPASS, //new!
+  input C_BYPASS,
   input C_EN,
-  input C_SRST_N, // if you remove this it break efpga_mult_regi...
-  input [39:0] P, // this is a problem fore sure
+  input C_SRST_N,
+  input [39:0] P,
   input P_BYPASS,
-  input P_EN,  // do not remove
+  input P_EN,
   input P_SRST_N
 );
 
@@ -38,14 +38,14 @@ generate
       .Y(P)
     );
   end
-  else if (BYPASS_A == 1'b0 && BYPASS_B == 1'b0 && BYPASS_P==1'b1 && POST_ADDER_STATIC==1'b0) begin
+  else if (BYPASS_A == 1'b0 && BYPASS_B == 1'b0 && BYPASS_P==1'b1 && POST_ADDER_STATIC==1'b0 && USE_FEEDBACK==1'b0) begin
     efpga_mult_rego _TECHMAP_REPLACE_ (
       .A(A),
       .B(B),
       .Y(P)
     );
   end
-  else if (BYPASS_A == 1'b1 && BYPASS_B == 1'b1 && BYPASS_P==1'b1 && POST_ADDER_STATIC==1'b0) begin
+  else if (BYPASS_A == 1'b1 && BYPASS_B == 1'b1 && BYPASS_P==1'b1 && POST_ADDER_STATIC==1'b0 && USE_FEEDBACK==1'b0) begin
     efpga_mult_regio _TECHMAP_REPLACE_ (
       .A(A),
       .B(B),
@@ -59,10 +59,10 @@ generate
       .Y(P)
     );
   end
-  else if (BYPASS_A == 1'b1 && BYPASS_B == 1'b1 && BYPASS_P==1'b0 && POST_ADDER_STATIC==1'b1) begin
+  else if (BYPASS_A == 1'b1 && BYPASS_B == 1'b1 && BYPASS_P==1'b0 && POST_ADDER_STATIC==1'b1 && USE_FEEDBACK==1'b0) begin
     efpga_mult_addc_regi _TECHMAP_REPLACE_ (
       .A(A),
-      .B(B), //TODO add c input
+      .B(B),
       .C(C),
       .Y(P)
     );
@@ -83,7 +83,7 @@ generate
       .Y(P)
     );
   end
-  else if (BYPASS_A == 1'b0 && BYPASS_B == 1'b0 && BYPASS_P==1'b0 && POST_ADDER_STATIC==1'b1) begin
+  else if (BYPASS_A == 1'b0 && BYPASS_B == 1'b0 && BYPASS_P==1'b0 && POST_ADDER_STATIC==1'b1 && USE_FEEDBACK==1'b0) begin
     efpga_mult_addc _TECHMAP_REPLACE_ (
       .A(A),
       .B(B),
@@ -91,7 +91,7 @@ generate
       .Y(P)
     );
   end
-  else if (BYPASS_A == 1'b0 && BYPASS_B == 1'b0 && BYPASS_P==1'b0 && POST_ADDER_STATIC==1'b1 && USE_FEEDBACK==1'b1 && MULT_HAS_REG == 1'b1) begin
+  else if (BYPASS_A == 1'b0 && BYPASS_B == 1'b0 && BYPASS_P==1'b1 && POST_ADDER_STATIC==1'b1 && USE_FEEDBACK==1'b1 && MULT_HAS_REG == 1'b1) begin
     efpga_macc_pipe _TECHMAP_REPLACE_ (
       .A(A),
       .B(B),
@@ -105,7 +105,7 @@ generate
       .Y(P)
     );
   end
-  else if (BYPASS_A == 1'b1 && BYPASS_B == 1'b1 /*&& BYPASS_C==1'b0*/ && BYPASS_P==1'b1 && POST_ADDER_STATIC==1'b1 && USE_FEEDBACK == 1'b1 && MULT_HAS_REG == 1'b0) begin
+  else if (BYPASS_A == 1'b1 && BYPASS_B == 1'b1 && BYPASS_P==1'b1 && POST_ADDER_STATIC==1'b1 && USE_FEEDBACK == 1'b1 && MULT_HAS_REG == 1'b0) begin
     efpga_macc_regi _TECHMAP_REPLACE_ (
       .A(A),
       .B(B),
