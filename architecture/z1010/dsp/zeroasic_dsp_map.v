58b1435febe3f120119e6023a2e6156892e612b7
Fred Tombs <fred@zeroasic.com>
2025-09-15 15:04:57 -0400

    Signed multiplies map to soft logic

--- architecture/z1010/dsp/zeroasic_dsp_map.v
+++ architecture/z1010/dsp/zeroasic_dsp_map.v
@@ -11,7 +11,8 @@ module \$__MAE__
 		input [B_WIDTH-1:0] B,
 		output [Y_WIDTH-1:0] Y
 	);
-
+	generate
+	if(A_SIGNED == 0 && B_SIGNED == 0) begin
 	// Don't specify clock or reset.
 	MAE #(
 		.BYPASS_A(1'b0),
@@ -33,6 +34,12 @@ module \$__MAE__
 		.B_ARST_N(1'b1),
 		.C_ARST_N(1'b1),
 		.P_ARST_N(1'b1)
-
 	);
+	end else begin
+      // For signed multiplications, use a generic software-based multiplier
+      // that Yosys will break down into basic gates.
+      $__soft_mul #(.A_WIDTH(A_WIDTH), .B_WIDTH(B_WIDTH), .Y_WIDTH(Y_WIDTH), .A_SIGNED(A_SIGNED), .B_SIGNED(B_SIGNED))
+      soft_mult_inst (.A(A), .B(B), .Y(Y));
+    end
+	endgenerate
 endmodule
