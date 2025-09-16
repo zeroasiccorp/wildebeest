e30588ad34219168ef34bf1dc954366d9e2e0b42
tdbesson <thierry@zeroasic.com>
2025-09-16 09:22:50 +0200

    revert PR #43

--- src/synth_fpga.cc
+++ src/synth_fpga.cc
@@ -2629,12 +2629,8 @@ struct SynthFpgaPass : public ScriptPass
      run("stat");
 
      if(has_cell_type(yosys_get_design(), "\\MAE")) {
-       log_error("Could not techmap DSP to a valid configuration.\n");
-     }
-     
-     if(has_cell_type(yosys_get_design(), "$mul")) {
-       log_warning("Signed multiplies getting mapped to soft logic.\n");
-     }
+      log_error("Could not techmap DSP to a valid configuration.\n");
+   }
   }
 
   // -------------------------
